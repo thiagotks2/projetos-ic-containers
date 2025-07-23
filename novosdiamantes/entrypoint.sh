8e169c483a0f1f4c46d03e0792551cbf1560bcb3
Thiago Cardoso <silva.thiagocardoso@gmail.com>
2025-07-23 18:30:18 -0300

    funcionando o laravel new, com prob. versao mongo

--- novosdiamantes/entrypoint.sh
+++ novosdiamantes/entrypoint.sh
@@ -9,8 +9,9 @@ chown www-data:www-data /var/run/php
 export NVM_DIR="/root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Ativa o Laravel Installer
-export PATH="$PATH:/root/.composer/vendor/bin"
+# Ativa o Laravel Installer (caminho atualizado)
+export COMPOSER_HOME="/root/.config/composer"
+export PATH="/root/.config/composer/vendor/bin:$PATH"
 
 # Inicia o supervisor
 exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
