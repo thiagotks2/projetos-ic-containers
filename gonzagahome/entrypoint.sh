dbc622af450643c2a9d66e11c157c16f8c3ea50e
Thiago Cardoso <silva.thiagocardoso@gmail.com>
2025-09-05 11:29:25 -0300

    novos projetos adicionados axis, gonzaga-home, novosdiamantes e novotarefas

---
+++ gonzagahome/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "✅ Iniciando ambiente Novos Diamantes"
+
+mkdir -p /var/run/php
+chown www-data:www-data /var/run/php
+
+# Ativa NVM (se precisar usar Node depois)
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Ativa o Laravel Installer (caminho atualizado)
+export COMPOSER_HOME="/root/.config/composer"
+export PATH="/root/.config/composer/vendor/bin:$PATH"
+
+# Inicia o supervisor
+exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
--- novosdiamantes/mongo-init/init-mongo.js
+++ novosdiamantes/mongo-init/init-mongo.js
@@ -1,4 +1,4 @@
-db = db.getSiblingDB("228"); // Cria e usa o banco 228
+db = db.getSiblingDB("228");
 
 db.createUser({
     user: "site",
@@ -7,4 +7,5 @@ db.createUser({
         { role: "read", db: "228" }
     ]
 });
+
 db.banners.insertOne({ ativo: true, nome: "banner de exemplo" });
